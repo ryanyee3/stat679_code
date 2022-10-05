dbc1c2317bf0bf7a49999ebb7f06e3b47faa0db7
Kris Sankaran <sankaran.kris@gmail.com>
2022-10-05 14:02:33 -0500

    updated v0

--- examples/week5/week5-2/rain-v0.js
+++ examples/week5/week5-2/rain-v0.js
@@ -1,12 +1,14 @@
 
 function new_point(width, height, max_radius) {
   let generator = d3.randomUniform();
+  id += 1;
   return {
     x: width * generator(),
     y: height * generator(),
     r: 2,
     max_radius: max_radius * generator(),
-    rate: 1 + 0.1 * generator()
+    rate: 1 + 0.1 * generator(),
+    id: id
   }
 }
 
@@ -21,12 +23,14 @@ function update_vis() {
   let circ = d3.select("svg")
     .selectAll("circle")
     .data(rain)
-
-  circ.enter()
-    .append("circle")
-    .attrs({ cx: d => d.x,  cy: d.y})
-  circ.attr("r", d => d.r)
-  circ.exit().remove();
+    .join(
+      enter => enter.append("circle")
+        .attrs({ cx: d => d.x, cy: d => d.y }),
+      update => update.attr("r", d => d.r),
+      exit => exit.remove()
+    )
 }
 
-//d3.interval(update_vis, 100);
+let id = 0;
+let rain = [];
+d3.interval(update_vis, 100);
