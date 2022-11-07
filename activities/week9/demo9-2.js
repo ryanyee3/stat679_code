5ad7f52ecfda1bcb71b51630c51184d5a6fb3a86
Kris Sankaran <sankaran.kris@gmail.com>
2022-11-07 16:51:48 -0600

    change to starter

--- activities/week9/demo9-2.js
+++ activities/week9/demo9-2.js
@@ -1,8 +1,4 @@
 
-function radius(depth) {
-  return 10 * Math.exp(-.5 * depth)
-}
-
 function make_tree(data) {
   data["edges"].push({to: "flare", from: null});
   let stratifier = d3.stratify()
@@ -25,8 +21,7 @@ function visualize(data) {
     .append("path")
     .attrs({
       d: link_gen,
-      transform: "translate(0, 10)", // so doesn't go off page
-      "stroke-width": 0.05
+      transform: "translate(0, 10)" // so doesn't go off page
     })
 
   d3.select("#tree")
@@ -36,45 +31,41 @@ function visualize(data) {
     .attrs({
       cx: d => d.x,
       cy: d => d.y,
-      r: d => 10 * Math.exp(-.5 * d.depth), // decreasing sizes
-      transform: "translate(0, 10)",
+      r: d => radius(d.depth),
       fill: "#A0C3D9",
+      transform: "translate(0, 10)"
     })
 
-  // Use d3.Delaunay to delineate the nearest neighborhoods of each node
-  // Create a mouseover event listener to run each time the mouse is moved
+  neighborhoods = d3.Delaunay.from(tree.descendants().map(d => [d.x, d.y]))
+  d3.select("svg").on("mousemove", (ev) => update_labels(ev, neighborhoods, tree))
 }
 
-function focus_ids(cur_node) {
-  // get the IDs of the ancestor nodes
-  // get the IDs of the descendants
-  // concatenate them together
-}
-
-function update(ev, neighborhoods, tree) {
-  // Use the delaunay neighborhoods to figure out the currently selected node
-  // Get the ancestor and descendant IDs around the current node using the
-  // .ancestors() and .descendants() methods on the stratified JS
+function update_labels(ev, neighborhoods, tree) {
+  let pos = d3.pointer(ev),
+    ix = neighborhoods.find(pos[0], pos[1]),
+    cur_node = tree.descendants()[ix]
 
   d3.select("#tree")
     .selectAll("circle")
-    .transition().duration(100)
-    //.attrs({
-    // set the fill so that the highlighted nodes are a different color
-    //})
-
-  d3.select("#tree")
-    .selectAll("path")
-    .transition().duration(100)
-    // Change the path width depending on the selection
-    //.attr(...)
+    .transition()
+    .duration(100)
+    .attrs({
+      r: (d, i) => i == ix ? 2 * radius(d.depth) : radius(d.depth),
+      fill: (d, i) => i == ix ? "#364959" : "#A0C3D9"
+    })
 
   d3.select("#labels")
     .selectAll("text")
-    //.text(cur_node.id)
-    //.attr("transform", `translate(${cur_node.x}, ${cur_node.y})`)
+    .text(cur_node.id)
+    .attrs({
+      transform: `translate(${cur_node.x}, ${cur_node.y})`
+    })
+
 }
 
+function radius(depth) {
+  return 10 * Math.exp(-.5 * depth)
+}
 
 d3.json("https://raw.githubusercontent.com/krisrs1128/stat679_code/main/examples/week9/week9-1/flare.json")
   .then(visualize)
