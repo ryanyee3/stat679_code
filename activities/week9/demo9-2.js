34da9b64eeffcbde574182d63bb1ded409da48e9
Kris Sankaran <sankaran.kris@gmail.com>
2022-11-09 11:49:44 -0600

    demo with ancestors included

--- activities/week9/demo9-2.js
+++ activities/week9/demo9-2.js
@@ -21,7 +21,8 @@ function visualize(data) {
     .append("path")
     .attrs({
       d: link_gen,
-      transform: "translate(0, 10)" // so doesn't go off page
+      transform: "translate(0, 10)", // so doesn't go off page
+      "stroke-width": 0.05
     })
 
   d3.select("#tree")
@@ -40,27 +41,47 @@ function visualize(data) {
   d3.select("svg").on("mousemove", (ev) => update_labels(ev, neighborhoods, tree))
 }
 
+function focus_ids(cur_node) {
+    descendants = cur_node.descendants().map(d => d.id)
+    ancestors = cur_node.ancestors().map(d => d.id)
+    return ancestors.concat(descendants)
+}
+
+function highlight(d, i, ix, focus) {
+  if (i == ix) {
+    return 1
+  } else if (focus.indexOf(d.id) != -1) {
+    return 0
+  }
+  return -1
+}
+
 function update_labels(ev, neighborhoods, tree) {
   let pos = d3.pointer(ev),
     ix = neighborhoods.find(pos[0], pos[1]),
-    cur_node = tree.descendants()[ix]
+    cur_node = tree.descendants()[ix],
+    focus = focus_ids(cur_node)
 
   d3.select("#tree")
     .selectAll("circle")
-    .transition()
-    .duration(100)
+    .transition().duration(100)
     .attrs({
-      r: (d, i) => i == ix ? 2 * radius(d.depth) : radius(d.depth),
-      fill: (d, i) => i == ix ? "#364959" : "#A0C3D9"
+      r: (d, i) => {
+        let relevance = highlight(d, i, ix, focus)
+        return relevance == 1 ? 2 * radius(d.depth) : relevance == 0 ? radius(d.depth) : .5 * radius(d.depth)
+      },
+      fill: (d, i) => highlight(d, i, ix, focus) >= 0 ? "#364959" : "#A0C3D9"
     })
+
+  d3.select("#tree")
+    .selectAll("path")
+    .transition().duration(100)
+    .attr("stroke-width", d => focus.indexOf(d.target.id) == -1 ? 0.05 : 1)
 
   d3.select("#labels")
     .selectAll("text")
     .text(cur_node.id)
-    .attrs({
-      transform: `translate(${cur_node.x}, ${cur_node.y})`
-    })
-
+    .attr("transform", `translate(${cur_node.x}, ${cur_node.y})`)
 }
 
 function radius(depth) {
